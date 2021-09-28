7199e992c92d908379830982fbbbb80ff1fc3545
tanithust <tanithust1202@gmail.com>
2021-09-28 10:24:11 +0700

    inital commmit

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+import Customer from './pages/Customer'
+import Supplier from './pages/Supplier'
+import Products from './pages/Products'
+import Login from './pages/Login'
+import Error from './pages/Error'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Dashboard}/>
+                <Route exact path='/products' component={Products}/>
+                <Route exact path='/supplier' component={Supplier}/>
+                <Route exact path='/customer' component={Customer}/>
+                <Route exact path='/login' component={Login}/>
+                <Route path='*' component={Error}/>
+            </Switch>
+        </Router>
+    )
+}
+export default AppRouter
