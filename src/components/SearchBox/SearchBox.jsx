191bcbc2f20b9f41a5587f5e600e92047964e837
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-03-04 20:58:07 +0700

    Create component SearchBox

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { InputAdornment, Paper, TextField } from "@material-ui/core";
+import { SearchIcon, CancelIcon } from "@material-ui/icons";
+import { useState } from 'react';
+
+SearchBox.propTypes = {
+	placeholder: PropTypes.string,
+	onChange: PropTypes.func,
+	onSubmit: PropTypes.func,
+	query: PropTypes.string,
+	removeable: PropTypes.bool,
+	autoFocus: PropTypes.bool,
+	inputProps: PropTypes.object,
+};
+
+function SearchBox(props) {
+	const { placeholder = "", onChange, onSubmit, query, removeable, autoFocus, inputProps } = props;
+	const [ value, setValue ] = useState(query ? query : "");
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+		if(onChange) {
+			onChange(e, e.target.value);
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if(onSubmit) {
+			onSubmit(e, value);
+		}
+	};
+
+	const handleClear = (e) => {
+		if(onChange) {
+			onChange(e, "");
+		}
+	}
+
+	return (
+		<Paper component="form" onSubmit={handleSubmit}>
+			<TextField
+				placeholder={placeholder}
+				value={value}
+				onChange={handleChange}
+				fullWidth
+				autoFocus={autoFocus}
+				InputProps={{
+					startAdornment: (
+						<InputAdornment position='start'>
+							<SearchIcon style={{ width: "24px", height: "24px" }} />
+						</InputAdornment>
+					),
+					endAdornment: removeable && value && value.length > 0 (
+						<InputAdornment position='end' onClick={handleClear}>
+							<CancelIcon style={{ width: "24px", height: "24px" }} />
+						</InputAdornment>
+					),
+				}}
+				inputProps={inputProps}
+				variant="outlined"
+			/>
+		</Paper>
+	);
+}
+
+export default SearchBox;
