a52d951f2bd5ef6285a64f357d98fd82553fe39f
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-03-05 20:00:36 +0700

    Added content when open Popper

--- src/components/LiveSearch/LiveSearch.jsx
+++ src/components/LiveSearch/LiveSearch.jsx
@@ -4,34 +4,43 @@ import SearchBox from "../SearchBox/SearchBox";
 import { useState } from "react";
 import { useRef } from "react";
 import BudaPopper from "../Popper/BudaPopper";
-import { Box } from "@material-ui/core";
+import { Box, CircularProgress, List, ListItem, Paper, Typography } from "@material-ui/core";
+import { Button } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 LiveSearch.propTypes = {
+  data: PropTypes.array,
   handleRender: PropTypes.func,
   placeholder: PropTypes.string,
   createable: PropTypes.bool,
   textCreate: PropTypes.string,
   onClickCreate: PropTypes.func,
   inputProps: PropTypes.object,
+  height: PropTypes.number,
+  maxHeight: PropTypes.number,
 };
 
 function LiveSearch(props) {
   const {
+    data,
     handleRender,
     placeholder,
     createable,
     textCreate,
     onClickCreate,
     inputProps,
+    height,
+    maxHeight,
   } = props;
 
   const [query, setQuery] = useState("");
   const [popperOpen, setPopperOpen] = useState(false);
   const refInput = useRef(null);
 
-	const handleQueryChange = (e, value) => {
-		setQuery(value);
-	}
+  const handleQueryChange = (e, value) => {
+    setQuery(value);
+  };
 
   const handleOpenPopper = (e) => {
     if (popperOpen) {
@@ -50,7 +59,7 @@ function LiveSearch(props) {
       <SearchBox
         query={query}
         placeholder={placeholder}
-				onChange={handleQueryChange}
+        onChange={handleQueryChange}
         removeable
         inputProps={{
           ...inputProps,
@@ -62,10 +71,37 @@ function LiveSearch(props) {
         open={popperOpen}
         onClose={handleClosePopper}
       >
-				<Box sx={{ border: 1, marginTop: 10, p: 1, bgcolor: 'background.paper' }}>
-          The content of the Popper.
+        <Box>
+          {popperOpen && createable && (
+            <Button
+              startIcon={<AddCircleOutlineIcon color="primary" />}
+              onClick={onClickCreate}
+            >
+              <Typography variant="subtitle1" color="primary">
+                {textCreate}
+              </Typography>
+            </Button>
+          )}
+          {popperOpen && data && data.length > 0 ? (
+            <InfiniteScroll
+              dataLength={data.length}
+              loader={
+                <Box display="flex" alignItems="center" justifyContent="center" py={2}>
+                  <CircularProgress size={16} />
+                </Box>
+              }
+              height={height|| "auto"}
+              style={{ maxHeight: maxHeight }}
+            >
+              {data.map((dataItem, idx) =>
+                handleRender(dataItem)
+              )}
+            </InfiniteScroll>
+          ) : (
+            <div></div>
+          )}
         </Box>
-			</BudaPopper>
+      </BudaPopper>
     </div>
   );
 }
