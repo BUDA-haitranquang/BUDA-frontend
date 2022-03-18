4b2117e0ff88f0947ef31fe02aad2c21dcaa9df4
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-03-18 23:20:27 +0700

    Temporary commit for create BuyOrder

---
+++ src/pages/buyorder/create/components/BoxSupplier/BoxSupplier.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import LiveSearch from "../../../../../buda-components/livesearch/BudaLiveSearch";
+import AddSupplierModal from "../../../../../components/modal/AddSupplierModal";
+import SupplierDetail from "./SupplierDetail/SupplierDetail";
+import useStyles from "./BoxSupplier.styles";
+import { useQuery } from "@apollo/client";
+import { LOAD_SUPPLIERS } from "../../../../../graphQl/suppliers/suppliersQueries";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+
+function BoxSupplier(props) {
+  const [openCreateSupplier, setOpenCreateSupplier] = useState(false);
+  const [chosenSupplier, setChosenSupplier] = useState(null);
+  const [suppliers, setSuppliers] = useState([]);
+  const { data } = useQuery(LOAD_SUPPLIERS);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    async function fetchData() {
+      if (data) {
+        setSuppliers(data.suppliersByUser);
+      }
+    }
+    fetchData();
+  }, [data]);
+
+  const filterSupplier = (filter) => {
+    return suppliers.filter(
+      (supplier) =>
+        supplier.name.include(filter) || supplier.phoneNumber.include(filter)
+    );
+  };
+
+  const renderRowSupplier = (option) => {
+    return (
+      option && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Typography>{option.name}</Typography>
+          <Typography fontWeight="bold">{option.phoneNumber}</Typography>
+        </Box>
+      )
+    );
+  };
+
+  const onChooseSupplier = (option) => {
+    setChosenSupplier(option);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Box className="BoxSupplier-main">
+        <Box className="BoxSupplier-header">
+          <Typography variant="subtitle1" paddingBottom={24}>
+            Supplier detail info.
+          </Typography>
+          {!chosenSupplier ? (
+            <LiveSearch
+              placeholder="Search for a supplier"
+              createable
+              textCreate="Add a new supplier"
+              onClickCreate={() => setOpenCreateSupplier(true)}
+              height={200}
+              maxHeight={200}
+              onChooseItem={(item) => onChooseSupplier(item)}
+              fetchData={(filterRequest) => filterSupplier(filterRequest)}
+              handleRender={(option) => renderRowSupplier(option)}
+            />
+          ) : (
+            <Box>
+              <AccountCircleIcon sx={{ paddingRight: 16 }} />
+              <Typography variant="subtitle1" color="#08f">
+                {chosenSupplier.name}
+              </Typography>
+              <Typography color="black">
+                {chosenSupplier.phoneNumber}
+              </Typography>
+              <CancelOutlinedIcon sx={{ paddingLeft: 16 }} />
+            </Box>
+          )}
+        </Box>
+
+        <SupplierDetail supplier={chosenSupplier} />
+
+        <AddSupplierModal
+          isOpen={openCreateSupplier}
+          handleClose={() => setOpenCreateSupplier(false)}
+        />
+      </Box>
+    </div>
+  );
+}
+
+export default BoxSupplier;
---
+++ src/pages/buyorder/create/components/BoxSupplier/SupplierDetail/SupplierDetail.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box, Grid, Typography } from "@mui/material";
+import useStyles from "./SupplierDetail.styles";
+
+SupplierDetail.propTypes = {
+  supplier: PropTypes.object
+};
+
+function SupplierDetail(props) {
+  const { supplier } = props;
+
+  const classes = useStyles();
+
+  return (
+    supplier ? (
+      <Grid xs={1} md={4} spacing={3}>
+        <Box display="flex" justifyContent="space-between">
+          <Typography color="#747C87">Address</Typography>
+          <Typography>{supplier.address}</Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="space-between">
+          <Typography color="#747C87">Email</Typography>
+          <Typography>{supplier.address}</Typography>
+        </Box>
+      </Grid>
+    ) : (
+      <></>
+    )
+  );
+}
+
+export default SupplierDetail;
