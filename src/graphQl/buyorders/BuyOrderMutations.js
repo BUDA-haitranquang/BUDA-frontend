3cdcbe9c73a10fa0054ea4b6030909140bc595f7
hieutran29 <hieutt6@sapo.vn>
2022-04-03 11:47:30 +0700

    Done creating buy order

--- src/graphQl/buyorders/BuyOrderMutations.js
+++ src/graphQl/buyorders/BuyOrderMutations.js
@@ -1,5 +1,25 @@
 import { gql } from "@apollo/client";
 
+export const NEW_BUY_ORDER = gql `
+  mutation newBuyOrder(
+    $supplierID: Int,
+    $status: Status!,
+    $buyOrderItemDTOs: [BuyOrderItemDTO]!  
+  ) {
+    newBuyOrder(
+      newBuyOrder: {
+        supplier: {
+          supplierID: $supplierID
+        },
+        status: $status,
+        buyOrderItemDTOs: $buyOrderItemDTOs
+      }
+    ) {
+        buyOrderID
+    }
+  }
+`;
+
 export const DELETE_BUY_ORDER = gql`
   mutation deleteBuyOrder($buyOrderID: Int) {
     deleteBuyOrder(buyOrderID: $buyOrderID)
--- src/pages/buyorder/create/CreateBuyOrder.jsx
+++ src/pages/buyorder/create/CreateBuyOrder.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import BoxSupplier from "./components/BoxSupplier/BoxSupplier";
-import { Box, Grid, Toolbar } from "@mui/material";
+import { Box, Button, Grid, Toolbar } from "@mui/material";
 import BoxAdditionalInfo from "./components/BoxAdditionalInfo/BoxAdditionalInfo";
 import Sidebar from "../../../components/Sidebar";
 import BoxIngredient from "./components/BoxIngredient/BoxIngredient";
+import { CreateBuyOrderContext } from "./context/CreateBuyOrderContext";
+import { useMutation } from "@apollo/client";
+import { NEW_BUY_ORDER } from "../../../graphQl/buyorders/BuyOrderMutations";
 
 CreateBuyOrder.propTypes = {};
 
 function CreateBuyOrder(props) {
   const { window } = props;
+  const [buyOrderRequest, setBuyOrderRequest] = useState(null);
+  const [newBuyOrder] = useMutation(NEW_BUY_ORDER);
+
+  const handleCreateBuyOrder = async () => {
+    setBuyOrderRequest((prevBuyOrderRequest) => ({
+      ...prevBuyOrderRequest,
+      status: "RECEIVING",
+    }));
+    try {
+      await newBuyOrder({
+        variables: {
+          status: "RECEIVING",
+          buyOrderItemDTOs: buyOrderRequest.buyOrderItemDTOs.map((item) => {
+            return {
+              quantity: item.quantity,
+              pricePerUnit: item.pricePerUnit,
+              ingredient: {
+                ingredientID: item.ingredient.ingredientID,
+              },
+            }
+          }),
+          supplierID: buyOrderRequest.supplier.supplierID,
+        },
+      });
+    } catch (e) {
+    }
+  }
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -24,17 +54,21 @@ function CreateBuyOrder(props) {
       >
         <Toolbar />
         <Box padding={3} width="100%">
-          <Grid container spacing={3} height={300} bgcolor="#f0f2f5">
-            <Grid item sm={12} md={9} maxHeight={300}>
-              <BoxSupplier />
+          <CreateBuyOrderContext.Provider value={{ buyOrderRequest, setBuyOrderRequest }}>
+            <Grid container spacing={3} bgcolor="#f0f2f5">
+              <Grid item sm={12} md={9} maxHeight={300}>
+                <BoxSupplier />
+              </Grid>
+              <Grid item sm={12} md={3} maxHeight={300}>
+                <BoxAdditionalInfo />
+              </Grid>
+              <Grid item xs={12}>
+                <BoxIngredient />
+              </Grid>
             </Grid>
-            <Grid item sm={12} md={3} maxHeight={300}>
-              <BoxAdditionalInfo />
-            </Grid>
-            <Grid item xs={12}>
-              <BoxIngredient />
-            </Grid>
-          </Grid>
+          </CreateBuyOrderContext.Provider>
+
+          <Button variant="contained" color={"success"} onClick={handleCreateBuyOrder}>Create</Button>
         </Box>
       </Box>
     </Box>
