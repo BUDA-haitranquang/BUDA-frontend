7f5088da223169ad51714914582760a6fcbed84e
HoangMinhBK <minhmangaka@gmail.com>
2022-04-28 09:39:52 +0700

    minor adjustment

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,13 +1,28 @@
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Badge, IconButton, Menu, MenuItem, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { useQuery } from "@apollo/client";
 import { GET_NOTIFICATION_BY_USER } from "../graphQl/notification/notificationByUser";
 import { useState, useEffect } from "react";
+export const useStyles = makeStyles((theme) => ({
+  menu: {
+    "& .MuiPaper-root": {
+      borderRadius: "10px",
+    },
+    marginLeft: "2rem",
+    height: "700px",
+  },
+}));
 const Notification = () => {
+  const cls = useStyles();
   const notificationData = useQuery(GET_NOTIFICATION_BY_USER);
-  // console.log(notificationData?.data?.notiByUser);
   const notiData = notificationData?.data?.notiByUser;
 
+  const unseenNoti = () => {
+    var count = 0;
+    for (let i = 0; i < notiData?.length; i++) if (!notiData[i]?.seen) count++;
+    return count;
+  };
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -19,27 +34,49 @@ const Notification = () => {
   return (
     <>
       <IconButton onClick={handleClick}>
-        <Badge badgeContent={notiData?.length} color="secondary">
+        {unseenNoti() > 0 ? (
+          <Badge badgeContent={unseenNoti()} color="secondary">
+            <NotificationsIcon style={{ color: "white" }} />
+          </Badge>
+        ) : (
           <NotificationsIcon style={{ color: "white" }} />
-        </Badge>
+        )}
       </IconButton>
-      <Menu
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        style={{ marginLeft: "2rem" }}
-      >
-        {notiData?.slice(0, 20).map((element, index) => (
-          <MenuItem key={index}>
-            <Box height="30px">{element.message}</Box>
+      {notiData ? (
+        <Menu
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          className={cls.menu}
+        >
+          {notiData?.map((element, index) => (
+            <MenuItem key={index}>
+              <Box height="30px">{element.message}</Box>
+            </MenuItem>
+          ))}
+        </Menu>
+      ) : (
+        <Menu
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          style={{ marginLeft: "2rem" }}
+        >
+          <MenuItem>
+            <Box height="30px">No notifications</Box>
           </MenuItem>
-        ))}
-      </Menu>
+        </Menu>
+      )}
     </>
   );
 };
