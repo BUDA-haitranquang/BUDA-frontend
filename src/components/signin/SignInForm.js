9d59c60f9f9c8794393370cd0995e712baf474cd
Tien4857 <tiennd@sapo.vn>
2022-07-06 20:04:31 +0700

    feat: add captcha when login

--- src/components/signin/SignInForm.js
+++ src/components/signin/SignInForm.js
@@ -1,30 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Box, Button, CircularProgress, InputAdornment, Link, Modal, OutlinedInput, Typography } from "@mui/material";
+import { useMutation } from "@apollo/client";
 import LockIcon from "@mui/icons-material/Lock";
 import PersonIcon from "@mui/icons-material/Person";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  InputAdornment,
+  Link,
+  Modal,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { useMutation } from "@apollo/client";
-import { useDispatch, useSelector } from "react-redux";
-import { addRefreshToken, addToken } from "../../redux/tokenSlice";
-
-import { LOGIN_USER, NEW_ACCESS_TOKEN } from "../../graphQl/authentication/authMutations";
-import { useHistory } from "react-router";
 import { useSnackbar } from "notistack";
-import { AlertErrorProp, AlertSuccessProp } from "../../buda-components/alert/BudaNoti";
+import React, { useEffect, useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import {
+  AlertErrorProp,
+  AlertSuccessProp,
+} from "../../buda-components/alert/BudaNoti";
+import { LOGIN_USER } from "../../graphQl/authentication/authMutations";
+import { addRefreshToken, addToken } from "../../redux/tokenSlice";
 
 const useStyle = makeStyles({
   wrapper: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   formContainer: {
     width: "100%",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   headlineText: {
     paddingTop: "15%",
@@ -33,25 +45,25 @@ const useStyle = makeStyles({
     color: "#fff",
     fontFamily: "Lexend Deca",
     fontWeight: 800,
-    marginLeft: "15%"
+    marginLeft: "15%",
   },
   outlinedInput: {
     "&.MuiOutlinedInput-root": {
       backgroundColor: "white",
       borderRadius: "10px",
       width: "100%",
-      height: "50px"
+      height: "50px",
     },
     "&.MuiOutlinedInput-inputAdornedStart": {
-      opacity: 0.5
+      opacity: 0.5,
     },
     "& input": {
       padding: "15px",
-      height: "10px"
+      height: "10px",
     },
     "& .MuiOutlinedInput-input": {
-      color: "#black"
-    }
+      color: "#black",
+    },
   },
   button1: {
     "&.MuiButton-root": {
@@ -63,13 +75,13 @@ const useStyle = makeStyles({
       boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
       "&:hover": {
         background: "rgba(97, 163, 255, 1)",
-        border: "none"
-      }
+        border: "none",
+      },
     },
     "&.MuiButton-text": {
-      fontSize: 19
-    }
-  }
+      fontSize: 19,
+    },
+  },
 });
 
 const SignInForm = () => {
@@ -81,9 +93,10 @@ const SignInForm = () => {
   const dispatch = useDispatch();
   const btn = useRef(null);
   const [userLogin, { loading, error }] = useMutation(LOGIN_USER);
-  const [newAccessToken] = useMutation(NEW_ACCESS_TOKEN);
-  const { refreshJwt } = useSelector((state) => state.token);
   const { enqueueSnackbar } = useSnackbar();
+  const [validCaptcha, setValidCaptcha] = useState(false);
+
+  const recaptchaRef = React.createRef();
 
   useEffect(() => {
     const listener = (event) => {
@@ -119,12 +132,22 @@ const SignInForm = () => {
     );
   //if (error) return `Sign in error! ${error.message}`;
 
+  // const executeCaptcha = () => { //used for invisible captcha
+  //   recaptchaRef.current.execute();
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validCaptcha === true) login();
+    else enqueueSnackbar("Wrong Captcha", AlertErrorProp);
+  };
+
   const login = () => {
     userLogin({
       variables: {
         email: email,
-        password: password
-      }
+        password: password,
+      },
     })
       .then((res) => {
         const { accessToken, refreshToken } = res.data.userLogin;
@@ -156,11 +179,6 @@ const SignInForm = () => {
   //   setTimeout(getNewAccessTokenLoop, 60);
   // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login();
-  };
-
   return (
     <>
       <Box className={classes.wrapper}>
@@ -169,7 +187,7 @@ const SignInForm = () => {
             width: "100%",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
           <Typography
@@ -178,7 +196,7 @@ const SignInForm = () => {
               fontFamily: "'Montserrat', sans-serif",
               color: "black",
               marginBottom: "4rem",
-              fontWeight: 500
+              fontWeight: 500,
             }}
           >
             BUDA
@@ -237,13 +255,21 @@ const SignInForm = () => {
             >
               LOG IN
             </Button>
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              // sitekey="6Lc7GssgAAAAANuBqTcUfs7qwjUdvjMn1QTSr7zg" //BUDA sitekey
+              sitekey="6LcIJ8sgAAAAAImCIDAolaNlAmWepkoYJ3CsF4qR"
+              onChange={() => setValidCaptcha(true)}
+              onExpired={() => setValidCaptcha(false)}
+            />
+
             <Typography
               style={{
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "center",
                 color: "black",
-                cursor: "default"
+                cursor: "default",
               }}
             >
               Don't have an account?&nbsp;
@@ -254,7 +280,7 @@ const SignInForm = () => {
                 style={{
                   color: "rgba(72, 149, 255, 1)",
                   textDecoration: "none",
-                  cursor: "pointer"
+                  cursor: "pointer",
                 }}
               >
                 Sign up
