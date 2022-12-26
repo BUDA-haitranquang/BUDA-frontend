5a95607aadf2941f5c4dcf90917558173c97467d
Tiennd4857 <tien.nd194857@sis.hust.edu.vn>
2022-12-26 23:43:24 +0700

    feat: update Product Statistics page

--- src/pages/Statistic/Product.jsx
+++ src/pages/Statistic/Product.jsx
@@ -1,72 +1,39 @@
-import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import { Divider, Toolbar, Typography } from "@mui/material";
 import { useQuery } from "@apollo/client";
+import Box from "@mui/material/Box";
+import { useEffect } from "react";
 import {
-  LOAD_RETENTION_RATE_MOTHLY,
-  LOAD_RETENTION_RATE_WEEKLY,
-} from "../../graphQl/statistics/statisticQueries";
-import BudaCircularChart from "../../buda-components/charts/BudaCircularChart";
+  PRODUCT_LEAST_PROFIT,
+  PRODUCT_LEAST_REVENUE,
+  PRODUCT_LEAST_SELL_NUMBER,
+  PRODUCT_MOST_RETURN_NUMBER,
+  PRODUCT_MOST_RETURN_PRICE,
+  PRODUCT_TOP_PROFIT,
+  PRODUCT_TOP_REVENUE,
+  PRODUCT_TOP_SELL_NUMBER,
+} from "src/graphQl/statistics/productQueries";
 
 const Retention = () => {
-  const { data: monthlyData } = useQuery(LOAD_RETENTION_RATE_MOTHLY);
-  const { data: weeklyData } = useQuery(LOAD_RETENTION_RATE_WEEKLY);
-  const [monthly, setMonthly] = useState(0);
-  const [weekly, setWeekly] = useState(0);
+  const { data: productsTopSellNumber } = useQuery(PRODUCT_TOP_SELL_NUMBER);
+  const { data: productsTopProfit } = useQuery(PRODUCT_TOP_PROFIT);
+  const { data: productsTopRevenue } = useQuery(PRODUCT_TOP_REVENUE);
+  const { data: productsLeastSellNumber } = useQuery(PRODUCT_LEAST_SELL_NUMBER);
+  const { data: productsLeastRevenue } = useQuery(PRODUCT_LEAST_REVENUE);
+  const { data: productsLeastProfit } = useQuery(PRODUCT_LEAST_PROFIT);
+  const { data: productsMostReturnNumber } = useQuery(
+    PRODUCT_MOST_RETURN_NUMBER
+  );
+  const { data: productsMostReturnPrice } = useQuery(PRODUCT_MOST_RETURN_PRICE);
 
   async function fetchData() {
-    if (monthlyData && weeklyData) {
-      setMonthly(monthlyData.retentionRateMonthly);
-      setWeekly(weeklyData.retentionRateWeekly);
-    }
   }
 
   useEffect(() => {
     fetchData();
-  }, [monthlyData, weeklyData]);
+  }, []);
 
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Box sx={{ width: "100%" }}>
-        <Toolbar />
-        <Typography
-          variant="caption"
-          component="div"
-          fontSize={32}
-          align="center"
-        >
-          Retention rate
-        </Typography>
-        <Box pt={1} sx={{ display: "flex", justifyContent: "space-evenly" }}>
-          <BudaCircularChart
-            value={weekly}
-            size="200px"
-            thickness={5}
-            title="Weekly"
-            color="error"
-          />
-          <BudaCircularChart
-            value={monthly}
-            size="200px"
-            thickness={5}
-            title="Monthly"
-            color="success"
-          />
-        </Box>
-        <Box py={1}></Box>
-        <Divider />
-        <Box
-          width="100%"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box py={1}>hello</Box>
-        </Box>
-      </Box>
-    </Box>
-  );
+  return <Box sx={{ display: "flex" }}>
+    Product Statistics
+  </Box>;
 };
 
 export default Retention;
