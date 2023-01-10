bea928a63e5bb17d43a724b5361d70040344c1d7
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-01-11 00:54:44 +0700

    feat: add information detail product statistics

--- src/pages/Statistic/Product.jsx
+++ src/pages/Statistic/Product.jsx
@@ -9,9 +9,16 @@ import {
   PRODUCT_MOST_RETURN_PRICE,
   PRODUCT_TOP_PROFIT,
   PRODUCT_TOP_REVENUE,
-  PRODUCT_TOP_SELL_NUMBER
+  PRODUCT_TOP_SELL_NUMBER,
 } from "src/graphQl/statistics/productQueries";
 import StatisticsCard from "./StatisticsCard";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import PaidIcon from "@mui/icons-material/Paid";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import SellIcon from "@mui/icons-material/Sell";
+import { Grid } from "@mui/material";
 
 const Retention = () => {
   const { data: productsTopSellNumber } = useQuery(PRODUCT_TOP_SELL_NUMBER);
@@ -20,36 +27,108 @@ const Retention = () => {
   const { data: productsLeastSellNumber } = useQuery(PRODUCT_LEAST_SELL_NUMBER);
   const { data: productsLeastRevenue } = useQuery(PRODUCT_LEAST_REVENUE);
   const { data: productsLeastProfit } = useQuery(PRODUCT_LEAST_PROFIT);
-  const { data: productsMostReturnNumber } = useQuery(PRODUCT_MOST_RETURN_NUMBER);
+  const { data: productsMostReturnNumber } = useQuery(
+    PRODUCT_MOST_RETURN_NUMBER
+  );
   const { data: productsMostReturnPrice } = useQuery(PRODUCT_MOST_RETURN_PRICE);
 
-  async function fetchData() {
-  }
+  async function fetchData() {}
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <Box sx={{ display: "flex" }}>
-    <StatisticsCard title={"Top Sell"}
-                    data={productsTopSellNumber?.productsTopSellNumber?.map(({ name, sellNumber }) => ({
-                      name, mainStats: sellNumber
-                    }))}
-                    sx={{ width: "30%" }}
-    />
-    <StatisticsCard title={"Top Revenue"}
-                    data={productsTopRevenue?.productsTopRevenue?.map(({ name, revenue }) => ({
-                      name, mainStats: revenue
-                    }))}
-                    sx={{ width: "30%" }}
-    />
-    <StatisticsCard title={"Top Profit"}
-                    data={productsTopProfit?.productsTopProfit?.map(({ name, profit }) => ({
-                      name, mainStats: profit
-                    }))}
-                    sx={{ width: "30%" }}
-    />
-  </Box>;
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Top Sell"}
+          data={productsTopSellNumber?.productsTopSellNumber?.map(
+            ({ name, sellNumber }) => ({
+              name,
+              mainStats: sellNumber,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="success"
+          icon={<TrendingUpIcon color="success" />}
+        />
+      </Grid>
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Top Revenue"}
+          data={productsTopRevenue?.productsTopRevenue?.map(
+            ({ name, revenue }) => ({
+              name,
+              mainStats: revenue,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="success"
+          icon={<SellIcon color="success" />}
+        />
+      </Grid>
+
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Top Profit"}
+          data={productsTopProfit?.productsTopProfit?.map(
+            ({ name, profit }) => ({
+              name,
+              mainStats: profit,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="success"
+          icon={<PaidIcon color="success" />}
+        />
+      </Grid>
+
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Most Returned"}
+          data={productsMostReturnNumber?.productsMostReturnNumber?.map(
+            ({ name, returnNumber }) => ({
+              name,
+              mainStats: returnNumber,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="error"
+          icon={<TrendingDownIcon color="error" />}
+        />
+      </Grid>
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Least Revenue"}
+          data={productsLeastRevenue?.productsLeastRevenue?.map(
+            ({ name, revenue }) => ({
+              name,
+              mainStats: revenue,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="error"
+          icon={<SellIcon color="error" />}
+        />
+      </Grid>
+
+      <Grid item xs={8} md={4}>
+        <StatisticsCard
+          title={"Least Profit"}
+          data={productsLeastProfit?.productsTopProfit?.map(
+            ({ name, profit }) => ({
+              name,
+              mainStats: profit,
+            })
+          )}
+          sx={{ width: "30%" }}
+          type="error"
+          icon={<PaidIcon color="error" />}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Retention;
